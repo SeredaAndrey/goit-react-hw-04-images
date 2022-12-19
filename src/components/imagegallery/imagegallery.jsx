92633c9fe32d630502d94a6f9d3e956222cec493
Andrii Sereda <a.a.sereda@ukr.net>
2022-12-19 12:45:54 +0200

    werfvwvwv

---
+++ src/components/imagegallery/imagegallery.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GalleryImage } from './imagegallery.styled';
+
+const ImageGallery = () => {
+  return <GalleryImage></GalleryImage>;
+};
+
+export default ImageGallery;
